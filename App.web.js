219fadbf8ec48b2582b4893829819dc6b3f41395
David Ruttka <david@davidruttka.com>
2021-04-10 13:01:18 -0700

    chore: integrate react-native-web

--- App.web.js
+++ App.web.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {View, Text, ActivityIndicator} from 'react-native';
 
 function App() {
   return (
-    <>
-      <h1>Hello world from react</h1>
-    </>
+    <View>
+      <Text>Hello world from react native</Text>
+      <ActivityIndicator />
+    </View>
   );
 }
 
